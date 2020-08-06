e1521b0c43c5b496755929905c15123a46b5ed8b
Ali Dehghani <ali.dehghani.g@gmail.com>
2020-08-07 04:14:57 +0430

    Adventuring Towards the Winden Caves!

---
+++ src/main/java/me/alidg/WindenBenchmark.java
@@ -0,0 +1,10 @@
+package me.alidg;
+
+import org.openjdk.jmh.Main;
+
+public class WindenBenchmark {
+
+    public static void main(String[] args) throws Exception {
+        Main.main(args);
+    }
+}
