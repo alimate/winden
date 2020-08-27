ecb64485ce590a7a247d60718b320023437db45b
Ali Dehghani <ali.dehghani.g@gmail.com>
2020-08-28 00:24:50 +0430

    Polish.

--- src/main/java/me/alidg/WindenBenchmark.java
+++ src/main/java/me/alidg/WindenBenchmark.java
@@ -3,9 +3,7 @@ package me.alidg;
 import org.openjdk.jmh.Main;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
@@ -13,7 +11,6 @@ import org.openjdk.jmh.annotations.State;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.LongStream;
 
 import static java.util.stream.Collectors.toList;
@@ -28,7 +25,7 @@ public class WindenBenchmark {
     private List<Long> sorted;
     private List<Long> shuffled;
 
-    @Setup(Level.Trial)
+    @Setup
     public void setup() {
         sorted = LongStream.range(0, length).boxed().collect(toList());
 
